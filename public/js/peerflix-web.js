b76d507fe91b8af5ec01caf3aa65c64830581707
Dimitris Zervas <dzervas@dzervas.gr>
2017-05-09 02:11:01 +0300

    Remove omx, basic changes to play on web & use popcorn-api

--- public/js/peerflix-web.js
+++ public/js/peerflix-web.js
@@ -1,124 +1,131 @@
 $(document).ready(function() {
 
-  var isPaused = false;
+	var isPaused = false;
 
-  (function poll() {
-    var firstRun = true;
-    var updateStatus = function() {
-      $.ajax({ url: 'status', success: function(status) {
-        if (status === 'PAUSED') {
-          $('#stop-wrapper').show();
-          $('#start-wrapper').hide();
-          $('#omx-controls').show();
-          $('#loader').hide();
-          if (firstRun) { // Show play button if already paused
-            showPauseIcon(false);
-            firstRun = false;
-          }
-        }
-        else if (status === 'IDLE') {
-          isPaused = true;
-          $('#omx-controls').hide();
-        }
-        else {
-          $('#stop-wrapper').show();
-          $('#start-wrapper').hide();
-          $('#omx-controls').show();
-          $('#loader').hide();
-          if (firstRun) { // Show pause button if already playing
-            showPauseIcon(true);
-            firstRun = false;
-          }
-        }
-      }, dataType: 'text' });
-    };
+	(function poll() {
+		var firstRun = true;
+		var updateStatus = function() {
+			$.ajax({ url: 'status', success: function(status) {
+				if (status === 'PAUSED') {
+					$('#stop-wrapper').show();
+					$('#start-wrapper').hide();
+					$('#omx-controls').show();
+					$('#loader').hide();
+					if (firstRun) { // Show play button if already paused
+						showPauseIcon(false);
+						firstRun = false;
+					}
+				}
+				else if (status === 'IDLE') {
+					isPaused = true;
+					$('#omx-controls').hide();
+				}
+				else {
+					$('#stop-wrapper').show();
+					$('#start-wrapper').hide();
+					$('#omx-controls').show();
+					$('#loader').hide();
+					if (firstRun) { // Show pause button if already playing
+						showPauseIcon(true);
+						firstRun = false;
+					}
+				}
+			}, dataType: 'text' });
+		};
 
-    updateStatus();
-    setInterval(function() {
-      updateStatus();
-    }, 5000);
-  })();
+		updateStatus();
+		//setInterval(function() {
+			//updateStatus();
+		//}, 5000);
+	})();
 
-  var showPauseIcon = function(paused) {
-    if (paused) {
-      $('#pause').removeClass('glyphicon-play');
-      $('#pause').addClass('glyphicon-pause');
-      isPaused = true;
-    }
-    else {
-      $('#pause').removeClass('glyphicon-pause');
-      $('#pause').addClass('glyphicon-play');
-      isPaused = false;
-    }
-  };
+	var showPauseIcon = function(paused) {
+		if (paused) {
+			$('#pause').removeClass('glyphicon-play');
+			$('#pause').addClass('glyphicon-pause');
+			isPaused = true;
+		}
+		else {
+			$('#pause').removeClass('glyphicon-pause');
+			$('#pause').addClass('glyphicon-play');
+			isPaused = false;
+		}
+	};
 
-  $('#start').click(function() {
-    $('#start-wrapper').hide();
-    $('#stop-wrapper').show();
-    $('#loader').show();
-    showPauseIcon(true);
-    var url = $('#torrent-url').val();
-    $('#torrent-url').val('');
-    $.post('play', { 'url': url })
-    .fail(function() {
-      $('#loader').hide();
-      $('#stop-wrapper').hide();
-      $('#start-wrapper').show();
-    });
-  });
+	$('#start').click(function() {
+		$('#start-wrapper').hide();
+		$('#stop-wrapper').show();
+		$('#loader').show();
+		showPauseIcon(true);
+		var url = $('#torrent-url').val();
+		$('#torrent-url').val('');
+		$.post('play', { 'url': url })
+			.done(function(res) {
+				window.open('http://' + location.hostname + ':' + res.port, '_blank');
+			})
+			.fail(function() {
+				$('#loader').hide();
+				$('#stop-wrapper').hide();
+				$('#start-wrapper').show();
+			});
+	});
 
-  $('#torrent-url').keydown(function(e) {
-    if (e.which === 13) { $('#start').click(); }
-  });
+	$('#torrent-url').keydown(function(e) {
+		if (e.which === 13) { $('#start').click(); }
+	});
 
-  $('#stop').click(function() {
-    $('#stop-wrapper').hide();
-    $('#start-wrapper').show();
-    $('#loader').hide();
-    showPauseIcon(true);
-    $.post('stop');
-  });
+	$('#stop').click(function() {
+		$('#stop-wrapper').hide();
+		$('#start-wrapper').show();
+		$('#loader').hide();
+		showPauseIcon(true);
+		$.post('stop');
+	});
 
-  $('#backward').click(function() {
-    $.post('backward');
-  });
+	$('#backward').click(function() {
+		$.post('backward');
+	});
 
-  $('#pause').click(function() {
-    showPauseIcon(!isPaused);
-    $.post('pause');
-  });
+	$('#pause').click(function() {
+		showPauseIcon(!isPaused);
+		$.post('pause');
+	});
 
-  $('#forward').click(function() {
-    $.post('forward');
-  });
+	$('#forward').click(function() {
+		$.post('forward');
+	});
 
-  $('#search-torrents').click(function() {
-    var searchStr = $('#torrent-query').val();
-    if (!searchStr.length) { $('#torrent-table').empty(); return; }
-    $.get('query', { 'q': searchStr })
-    .done(function(searchResults) {
-      $('#torrent-table').html(searchResults.length ? '<thead><tr><th width="80%">Title</th><th width="10%" style="text-align: center;"><span class="glyphicon glyphicon-menu-up"></span></th><th width="10%" style="text-align: center;"><span class="glyphicon glyphicon-menu-down"></span></th></tr></thead>' : '');
-      searchResults.forEach(function(result) {
-        var title = result.title;
-        var torrentLink = result.torrentLink;
-        var seeds = result.seeds;
-        var leechs = result.leechs;
-        $('#torrent-table').append('<tr><td style="word-break: break-all;"><a class="torrent-link" href="' + torrentLink + '">' + title + '</a></td><td style="text-align: center;"><span style="color:#00CB95;">' + seeds + '</span></td><td style="text-align: center;"><span style="color:#FF646C;">' + leechs + '</span></td></tr>');
-      });
-      if (!searchResults.length) { $('#torrent-table').empty(); }
-    })
-    .fail(function() {
-      $('#torrent-table').empty();
-    });
-  });
+	$('#search-torrents').click(function() {
+		var searchStr = $('#torrent-query').val();
+		if (!searchStr.length) { $('#torrent-table').empty(); return; }
+		$.get('https://tv-v2.api-fetch.website/movies/1', { 'keywords': searchStr })
+			.done(function(searchResults) {
+				$('#torrent-table').html(searchResults.length ? '<thead><tr><th width="80%">Title</th><th width="10%" style="text-align: center;"><span class="glyphicon glyphicon-menu-up"></span></th><th width="10%" style="text-align: center;"><span class="glyphicon glyphicon-menu-down"></span></th></tr></thead>' : '');
+				searchResults.forEach(function(result) {
+					var title = result.title;
+					var quality = result.torrents.en;
+					Object.keys(quality).forEach(function(key) {
+						var torrentLink = quality[key].url;
+						var seeds = quality[key].seed;
+						var leechs = quality[key].peer;
+						var filesize = quality[key].filesize;
+						$('#torrent-table').append('<tr><td style="word-break: break-all;"><a class="torrent-link" href="' + torrentLink + '">[' + key + '] ' + title + ' - ' + filesize + '</a></td><td style="text-align: center;"><span style="color:#00CB95;">' + seeds + '</span></td><td style="text-align: center;"><span style="color:#FF646C;">' + leechs + '</span></td></tr>');
+					});
+				});
+				if (!searchResults.length) { $('#torrent-table').empty(); }
+			})
+			.fail(function() {
+				$('#torrent-table').empty();
+			});
+	});
 
-  $('#torrent-table').on('click', '.torrent-link', function(e) {
-    e.preventDefault();
-    $('#torrent-url').val( $(this).attr('href') );
-    $('#start').click();
-  });
+	$('#torrent-table').on('click', '.torrent-link', function(e) {
+		e.preventDefault();
+		$('#torrent-url').val( $(this).attr('href') );
+		$('#start').click();
+	});
 
-  $('#torrent-query').keydown(function(e) {
-    if (e.which === 13) { $('#search-torrents').click(); }
-  });
+	$('#torrent-query').keydown(function(e) {
+		if (e.which === 13) { $('#search-torrents').click(); }
+	});
 });
--- server.js
+++ server.js
@@ -15,7 +15,6 @@ var uuid = require('node-uuid');
 var fs = require('fs');
 var kickass = require('kickass-torrent');
 var peerflix = require('peerflix');
-var omx = require('omxctrl');
 
 // Configs
 var PORT = process.env.PORT || process.argv[2] || 8080;
@@ -24,44 +23,27 @@ var LOG_ENABLED = true;
 // Params
 var connection;
 var states = ['PLAYING', 'PAUSED', 'IDLE'];
-var omxCtrlMap = {
-  'pause': 'pause',
-  'speedup': 'increaseSpeed',
-  'speeddown': 'decreaseSpeed',
-  'nextaudio': 'nextAudioStream',
-  'prevaudio': 'previousAudioStream',
-  'nextsubtitle': 'nextSubtitleStream',
-  'prevsubtitle': 'previousSubtitleStream',
-  'togglesubtitle': 'toggleSubtitles',
-  'volumeup': 'increaseVolume',
-  'volumedown': 'decreaseVolume',
-  'forward': 'seekForward',
-  'backward': 'seekBackward',
-  'fastforward': 'seekFastForward',
-  'fastbackward': 'seekFastBackward'
-};
 
 // Helper Methods
 var clearTorrentCache = function() {
-  fs.readdir(tempDir, function(err, files) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    files.forEach(function(file) {
-      if (file.substr(0, 9) === 'peerflix-') {
-        rimraf.sync(path.join(tempDir, file));
-      }
-    });
-  });
+	fs.readdir(tempDir, function(err, files) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		files.forEach(function(file) {
+			if (file.substr(0, 9) === 'peerflix-') {
+				rimraf.sync(path.join(tempDir, file));
+			}
+		});
+	});
 };
 
 var stop = function() {
-  clearTorrentCache();
-  if (!connection) { return; }
-  connection.destroy();
-  connection = null;
-  omx.stop();
+	clearTorrentCache();
+	if (!connection) { return; }
+	connection.destroy();
+	connection = null;
 };
 
 // Server Setup
@@ -69,126 +51,116 @@ var server = new Hapi.Server();
 server.connection({ port: PORT });
 
 if (LOG_ENABLED) {
-  var options = { logRequestPayload: true };
-  var opsPath = path.normalize(__dirname +  '/log/operation');
-  var errsPath = path.normalize(__dirname + '/log/error');
-  var reqsPath = path.normalize(__dirname + '/log/request');
-  mkdirp.sync(opsPath);
-  mkdirp.sync(errsPath);
-  mkdirp.sync(reqsPath);
-  var configWithPath = function(path) {
-    return { path: path, extension: 'log', rotate: 'daily', format: 'YYYY-MM-DD-X', prefix:'peerflix-web' };
-  };
-  var consoleReporter = new GoodConsole({ log: '*', response: '*' });
-  var opsReporter = new GoodFile(configWithPath(opsPath), { log: '*', ops: '*' });
-  var errsReporter = new GoodFile(configWithPath(errsPath), { log: '*', error: '*' });
-  var reqsReporter = new GoodFile(configWithPath(reqsPath), { log: '*', response: '*' });
-  options.reporters = [ consoleReporter, opsReporter, errsReporter, reqsReporter ];
-  server.register({ register: Good, options: options}, function(err) { if (err) { throw(err); } });
+	var options = { logRequestPayload: true };
+	var opsPath = path.normalize(__dirname +  '/log/operation');
+	var errsPath = path.normalize(__dirname + '/log/error');
+	var reqsPath = path.normalize(__dirname + '/log/request');
+	mkdirp.sync(opsPath);
+	mkdirp.sync(errsPath);
+	mkdirp.sync(reqsPath);
+	var configWithPath = function(path) {
+		return { path: path, extension: 'log', rotate: 'daily', format: 'YYYY-MM-DD-X', prefix:'peerflix-web' };
+	};
+	var consoleReporter = new GoodConsole({ log: '*', response: '*' });
+	var opsReporter = new GoodFile(configWithPath(opsPath), { log: '*', ops: '*' });
+	var errsReporter = new GoodFile(configWithPath(errsPath), { log: '*', error: '*' });
+	var reqsReporter = new GoodFile(configWithPath(reqsPath), { log: '*', response: '*' });
+	options.reporters = [ consoleReporter, opsReporter, errsReporter, reqsReporter ];
+	server.register({ register: Good, options: options}, function(err) { if (err) { throw(err); } });
 }
 
 server.start(function () {
-  clearTorrentCache();
-  console.log('Peerflix web running at: http://' + ip.address() + ':' + server.info.port);
+	clearTorrentCache();
+	console.log('Peerflix web running at: http://' + ip.address() + ':' + server.info.port);
 });
 
 // Routes
 server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    return reply.file(path.join(__dirname, 'public/index.html'));
-  }
+	method: 'GET',
+	path: '/',
+	handler: function (request, reply) {
+		return reply.file(path.join(__dirname, 'public/index.html'));
+	}
 });
 
 server.route({
-  method: 'GET',
-  path: '/assets/{param*}',
-  handler: {
-    directory: {
-      path: path.join(__dirname,'public')
-    }
-  }
+	method: 'GET',
+	path: '/assets/{param*}',
+	handler: {
+		directory: {
+			path: path.join(__dirname,'public')
+		}
+	}
 });
 
 server.route({
-  method: 'POST',
-  path: '/play',
-  handler: function (request, reply) {
-    var torrentUrl = request.payload.url;
-    if (torrentUrl) {
-      readTorrent(torrentUrl, function(err, torrent) {
-        if (err) { return reply(Boom.badRequest(err)); }
-        if (connection) { stop(); }
+	method: 'POST',
+	path: '/play',
+	handler: function (request, reply) {
+		var torrentUrl = request.payload.url;
+		if (torrentUrl) {
+			readTorrent(torrentUrl, function(err, torrent) {
+				if (err) { return reply(Boom.badRequest(err)); }
+				if (connection) { stop(); }
 
-        connection = peerflix(torrent, {
-          connections: 100,
-          path: path.join(tempDir, 'peerflix-' + uuid.v4()),
-          buffer: (1.5 * 1024 * 1024).toString()
-        });
+				connection = peerflix(torrent, {
+					connections: 100,
+					path: path.join(tempDir, 'peerflix-' + uuid.v4()),
+					buffer: (1.5 * 1024 * 1024).toString()
+				});
 
-        connection.server.on('listening', function() {
-          if (!connection) { return reply(Boom.badRequest('Stream was interrupted')); }
-          omx.play('http://127.0.0.1:' + connection.server.address().port + '/');
-          omx.on('ended', function() { stop(); });
-          return reply();
-        });
-      });
-    }
-    else {
-      return reply(Boom.badRequest('Torrent URL Required'));
-    }
-  }
+				connection.server.once('error', function() {
+					engine.server.listen(0);
+				});
+
+				connection.server.on('listening', function() {
+					if (!connection) { return reply(Boom.badRequest('Stream was interrupted')); }
+					console.log(connection.server.address());
+					return reply({ port: connection.server.address().port });
+				});
+			});
+		}
+		else {
+			return reply(Boom.badRequest('Torrent URL Required'));
+		}
+	}
 });
 
 server.route({
-  method: 'POST',
-  path: '/stop',
-  handler: function (request, reply) {
-    stop();
-    return reply();
-  }
+	method: 'POST',
+	path: '/stop',
+	handler: function (request, reply) {
+		stop();
+		return reply();
+	}
 });
 
 server.route({
-  method: 'GET',
-  path: '/status',
-  handler: function (request, reply) {
-    return reply(states[omx.getState()]);
-  }
+	method: 'GET',
+	path: '/status',
+	handler: function (request, reply) {
+		return reply(
+		);
+	}
 });
 
 server.route({
-  method: 'GET',
-  path: '/query',
-  handler: function (request, reply) {
-    var query = request.query.q;
-    if (query) {
-      kickass(query, function(err, response){
-        if (err) { return reply(Boom.badRequest(err)); }
-        var filteredResults = [];
-        response.list.forEach(function(result) {
-          if (result.category === 'TV' || result.category === 'Movies') {
-            filteredResults.push(result);
-          }
-        });
-        return reply(filteredResults);
-      });
-    }
-    else { return reply(Boom.badRequest('Torrent query string must be present')); }
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/{omx_command}',
-  handler: function (request, reply) {
-    var omxCommand = request.params.omx_command;
-    var actualCommand = omxCtrlMap[omxCommand];
-    if (actualCommand) {
-      omx[actualCommand]();
-      return reply();
-    }
-    else { return reply(Boom.badRequest('Invalid OMX Player command')); }
-  }
+	method: 'GET',
+	path: '/query',
+	handler: function (request, reply) {
+		var query = request.query.q;
+		if (query) {
+			kickass(query, function(err, response){
+				if (err) { return reply(Boom.badRequest(err)); }
+				var filteredResults = [];
+				response.list.forEach(function(result) {
+					if (result.category === 'TV' || result.category === 'Movies') {
+						filteredResults.push(result);
+					}
+				});
+				return reply(filteredResults);
+			});
+		}
+		else { return reply(Boom.badRequest('Torrent query string must be present')); }
+	}
 });
